984dc608cfc664d92c995f4622aa0e1972704805
aokik-ao <61224695+aokik-ao@users.noreply.github.com>
2022-05-25 14:57:49 +0900

    buildして表示されたのを確認

--- application/views/index.php
+++ application/views/index.php
@@ -5,15 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ここも変えられる？</title>
+    <title>react crud ts</title>
     <!--TODO 以下の外部ファイルをPHP側で取得するようにして、buildの度に書き換えなくても済むようにする必要あり -->
-    <link rel="stylesheet" href="<?= site_url('build/static/css/main.4876d760.css?') ?>">
-    <script type="module" src="<?= site_url('build/static/js/main.cc44c020.js?') ?>"></script>
+    <link rel="stylesheet" href="<?= site_url('build/static/css/main.abd64e84.css?') ?>">
+    <script type="module" src="<?= site_url('build/static/js/main.f1f16263.js?') ?>"></script>
 </head>
 
 <body>
     <div id="root">
-    </div>
-</body>
+        </div>
+    </body>
 
 </html>
